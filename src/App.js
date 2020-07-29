3866895fb117c724305e44d5bd0311dfcd5bf529
femzykel <femmzykel@gmail.com>
2020-07-29 03:57:35 -0600

    adding header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
-    <Switch>
-      <div>
+    <div>
+      <Header />
+      <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
-      </div>
-    </Switch>
+      </Switch>
+    </div>
   );
 }
 
